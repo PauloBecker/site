e225f9937b6791dc970cf1d842c25553f078c241
Paulo Becker <beckerpaulo81@gmail.com>
2026-02-20 15:01:32 -0300

    Add API CEP e melhorias no projeto.

--- script.js
+++ script.js
@@ -2,11 +2,34 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
+  const cepInput = document.getElementById("cep");
+
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     form.reset();
     alert("Mensagem enviada com sucesso!");
 });
+
+    cepInput.addEventListener("blur", () => {
+    const cep = cepInput.value.replace(/\D/g, ""); // remove caracteres não numéricos
+    if (cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+          if (!data.erro) {
+            document.getElementById("rua").value = data.logradouro;
+            document.getElementById("bairro").value = data.bairro;
+            document.getElementById("cidade").value = data.localidade;
+            document.getElementById("uf").value = data.uf;
+          } else {
+            alert("CEP não encontrado!");
+          }
+        })
+        .catch(() => alert("Erro ao consultar o CEP."));
+    } else {
+      alert("CEP inválido. Digite 8 números.");
+    }
+  });
 
 
   // Alternar tema claro/escuro
